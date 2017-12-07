932e7cabfdef40a367ec513510ca65fa017e4c6c
Raineer <delaritaraineer81@gmail.com>
2017-12-07 14:17:01 +0800

    angularlist

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList 
+} from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../services/auth.service';
@@ -9,7 +10,21 @@ import { ChatMessage } from '../models/chat-message.model';
 
 @Injectable()
 export class ChatService {
+	chatMessages: AngularFireList<ChatMessage[]>;
+	chatMessage: ChatMessage;
 
   constructor() { }
+
+  sendMessage(msg: string) {
+		const timestamp = this.getTimeStamp(); 
+		const email = this.user.email;
+			this.chatMessages = this.getMessages();
+			this.chatMessages.push({
+				message: msg,
+				timeSent: timestamp,
+				userName: this.userName,
+				email: email
+			});
+  }
 
 }
